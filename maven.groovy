a74d67485e2e1ab746d997e3eb0f6365716a5f7e
lugar52 <37659063+lugar52@users.noreply.github.com>
2020-12-19 06:50:14 -0300

    Update maven.groovy

--- maven.groovy
+++ maven.groovy
@@ -7,33 +7,33 @@
 def call(){
   
        stage('compile_code'){
-            steps {
+            
                     bat './mvnw.cmd clean compile -e'
-            }
+            
         }
         stage('test_code'){
-            steps {
+            
                     bat './mvnw.cmd clean test -e'
-            }
+            
         }
         stage('jar_code'){
-            steps {
+            
                     bat './mvnw.cmd clean package -e'
-            }
+            
         }
         stage('sonarQube') {
-            steps {
+            
                 withSonarQubeEnv(installationName: 'Sonar') 
                 { // You can override the credential to be used
                     bat 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
                 }
-            }
+            
         }
          stage('uploadNexus'){
-             steps {
+            
                  // logica para subir un artefacto a nexus
                  nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:\\Users\\Luis Garrido\\Desktop\\Devops\\ejemplo-maven\\ejemplo-maven\\build\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'spring-boot-starter-parent', groupId: 'org.springframework.boot', packaging: 'jar', version: '0.0.1']]]
-             }
+            
         }
 
 }
